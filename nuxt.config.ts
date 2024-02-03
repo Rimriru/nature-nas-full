d71eb41af2a25a5dcc5e60988dfd33da9b453406
Rimriru <ororonya@gmail.com>
2024-02-03 19:53:39 +0300

    feat: add a form for creating new page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Институт природопользования НАН Беларуси',
+      // meta: [{ name: 'description', content: 'My amazing site.' }],
+      // bodyAttrs: {
+      //   class: 'test'
+      // },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1'
+    }
+  },
   devtools: { enabled: true },
   runtimeConfig: {
     mongoDbUrl: process.env.MONGODB_URL
   },
   nitro: {
-    plugins: ["~/server/index.ts"]
+    plugins: ['~/server/index.ts']
   }
-})
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,49 @@
 <script setup lang="ts">
 import Editor from '@tinymce/tinymce-vue';
-// const users = ref({});
-const username = ref('');
-const password = ref('');
-
-// const fetchUsers = async () => {
-//   const { data } = await useFetch('/api/users');
-//   users.value = data;
-// };
-
-// fetchUsers();
-
-const handleSubmit = async () => {
-  useFetch('/api/users', {
-    method: 'POST',
-    body: {
-      username: username.value,
-      password: password.value
-    }
-  });
-  
-  // fetchUsers();
-  username.value = '';
-  password.value = '';
-}
-
+const route = ref('');
+const canvas = ref('');
 </script>
 
 <template>
-  <div class="content">{{username}}
-    <form @submit.prevent="handleSubmit">
-      <label>Username
-        <input v-model="username" type="text">
+  <div class="content">
+    <form novalidate>
+      <label class="route-input"
+        >http://www.nature-nas.by/
+        <input v-model="route" type="text" placeholder="Название страницы латинницей" required />
+        <span class="required">*</span>
       </label>
-      <br>
-      <label>Password
-        <input v-model="password" type="text">
-      </label>
-      <br>
-      <button type="submit">Submit</button>
+      <fieldset>
+        <p>Выберите шаблон страницы:</p>
+        <label>
+          <input v-model="canvas" type="radio" value="canvas1" name="canvas" checked />
+        </label>
+        <label>
+          <input v-model="canvas" type="radio" value="canvas2" name="canvas" />
+        </label>
+        <label>
+          <input v-model="canvas" type="radio" value="canvas3" name="canvas" />
+        </label>
+        <label>
+          <input v-model="canvas" type="radio" value="canvas4" name="canvas" />
+        </label>
+        <label>
+          <input v-model="canvas" type="radio" value="canvas5" name="canvas" />
+        </label>
+        <label>
+          <input v-model="canvas" type="radio" value="canvas6" name="canvas" />
+        </label>
+      </fieldset>
+      <button type="submit">Создать страницу</button>
     </form>
     <div class="editor">
-      <Editor 
-      api-key="j9zmlsfscynrcssyawis2dp00r22qej7ry4srjvz4k06rbo6"
-      :init="{
-        plugins: 'lists link image table code help wordcount autolink autosave media preview',
-        toolbar_mode: 'sliding',
-        language: 'ru'
-      }"
-    />
+      <Editor
+        api-key="j9zmlsfscynrcssyawis2dp00r22qej7ry4srjvz4k06rbo6"
+        :init="{
+          plugins: 'lists link image table code help wordcount autolink autosave media preview',
+          toolbar_mode: 'sliding',
+          language: 'ru'
+        }"
+      />
     </div>
   </div>
 </template>
@@ -56,6 +51,27 @@ const handleSubmit = async () => {
 <style>
 .content {
   text-align: center;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+}
+
+.required {
+  color: red;
+}
+
+.route-input input {
+  width: clamp(100px, 15vw, 230px);
+  padding-block: 8px;
+  border-radius: 10px;
+  border: none;
+  outline: black solid 1px;
+  margin-bottom: 30px;
+  margin-right: 5px;
+}
+
+fieldset {
+  border: none;
 }
 
 .editor {
