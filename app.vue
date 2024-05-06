9dee1543b4596a4a29acbe7c5cee02d965b79243
Rimriru <ororonya@gmail.com>
2024-05-06 19:04:10 +0300

    refactor: implement conditional routes and link groups state change

--- app.vue
+++ app.vue
@@ -8,21 +8,26 @@ const newsState = useNewsState();
 const contactsState = useContactsState();
 const loggedInState = useLoggedInState();
 
-const routes = await useAllRoutes();
-const linkGroups = await useLinkGroups();
+if (!routesState.value.length) {
+  const routes = await useAllRoutes();
+  routesState.value = routes;
+}
+
+if (!linkGroupsState.value.length) {
+  const linkGroups = await useLinkGroups();
+  linkGroupsState.value = linkGroups;
+}
+
 const photoFromDb = await useMainPhotoRequest();
 const news = await useNewsRequest();
 const contacts = await useContacts();
 
 const router = useRouter();
-
-routesState.value = routes;
-linkGroupsState.value = linkGroups;
 photoState.value = photoFromDb;
 newsState.value = news;
 contactsState.value = contacts;
 
-routes.forEach((route) => {
+routesState.value.forEach((route) => {
   router.addRoute({
     path: route.path,
     name: route.name,
