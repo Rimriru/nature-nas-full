21908b3c82c965852dfde6f6a69f2e84f7772431
Rimriru <ororonya@gmail.com>
2024-05-18 19:33:48 +0300

    fix: check for auth when patch journal

--- server/api/journal/[id].patch.ts
+++ server/api/journal/[id].patch.ts
@@ -1,18 +1,21 @@
 import { journal } from '~/server/models';
 
-export default defineEventHandler(async (event) => {
-  const id = getRouterParam(event, 'id');
-  const body = await readBody(event);
+export default defineEventHandler({
+  onRequest: [auth],
+  handler: async (event) => {
+    const id = getRouterParam(event, 'id');
+    const body = await readBody(event);
 
-  try {
-    const editedJournal = await journal
-      .findByIdAndUpdate(id, body, { new: true })
-      .populate(['authorRules', 'editorialPolicy']);
-    return editedJournal;
-  } catch (error: any) {
-    return createError({
-      status: error.statusCode,
-      message: error.message
-    });
+    try {
+      const editedJournal = await journal
+        .findByIdAndUpdate(id, body, { new: true })
+        .populate(['authorRules', 'editorialPolicy']);
+      return editedJournal;
+    } catch (error: any) {
+      return createError({
+        status: error.statusCode,
+        message: error.message
+      });
+    }
   }
 });
