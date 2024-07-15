3405248b3dec6e0aec7fa38e5df8a5b55ec31a9c
Rimriru <ororonya@gmail.com>
2024-07-15 18:45:09 +0300

    feat: add monographs page and route for year

---
+++ pages/monographs/[year].vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const route = useRoute();
+const { year: selectedYear } = route.params;
+
+const monographsState = useMgraphsState();
+// collect all monographs, display only with that year
+
+const selectedYearMonographs = computed(() => {
+  return monographsState.value.filter((graph) => graph.year === selectedYear);
+});
+</script>
+
+<template>
+  <ul class="monographs__container">
+    <li v-for="graph of selectedYearMonographs" :key="graph._id">
+      {{ graph.title }}
+    </li>
+  </ul>
+</template>
+
+<style lang="scss">
+.monographs__container {
+  flex-grow: 1;
+}
+</style>
